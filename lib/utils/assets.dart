e7794335113d7f86d137e0c71582e2a3582ad25e
Ghabriel <ghabriel@imdigital.com>
2025-04-14 15:39:20 -0300

    feat: Add nova belinha

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,6 +1,6 @@
 class Assets {
   static const background = "bg.png";
-  static const abelha = "abelha-cutout.png";
+  static const abelha = "abelha-cutout-g.png";
   static const enemy = "enemy.png";
   static const explosion = "explosion.png";
   static const polen = "polen.png";
